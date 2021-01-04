6319579a4d1d4d6e0c57d8ac04e39cb97cebb0e3
shensju <shensju@163.com>
2021-01-04 23:17:51 +0800

    update 0009 isPalindrome

--- src/Solution0009.java
+++ src/Solution0009.java
@@ -4,25 +4,19 @@
  */
 public class Solution0009 {
     public boolean isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
-            return false;
+        if (x < 0) return false;
+        int temp = x;
+        int help = 1;
+        while (temp >= 10) {
+            temp /= 10;
+            help *= 10;
         }
-        if (x < 10) {
-            return true;
-        }
-        int len = (int) (Math.log10(x) + 1);
-        if (len < 0) {
-            return false;
-        }
-        int[] arr = new int[len];
-        for (int i = 1; i <= len; i++) {
-            arr[i-1] = x % 10;
-            x /= 10;
-        }
-        for (int i = 0, j = len - 1; i != j && i < j; i++, j--) {
-            if (arr[i] != arr[j]) {
+        while (x != 0) {
+            if (x % 10 != x / help) {
                 return false;
             }
+            x = x % help / 10;
+            help /= 100;
         }
         return true;
     }
