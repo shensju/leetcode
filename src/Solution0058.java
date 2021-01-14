2688086b7a280f0bfbe67cf2e773835197f62e5d
shensju <shensju@163.com>
2021-01-14 22:17:58 +0800

    update 0058 Length of Last Word

--- src/Solution0058.java
+++ src/Solution0058.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 public class Solution0058 {
 
     /**0058 Length of Last Word**/
-    public static int lengthOfLastWord(String s) {
+    public int lengthOfLastWord(String s) {
         int i = s.length() - 1;
         int l = -1;
         int r = -1;
@@ -16,6 +16,7 @@ public class Solution0058 {
             if (!flag && !(s.charAt(i) == 32)){
                 r = i;
                 flag = true;
+                continue;
             }
             if (flag) {
                 if (s.charAt(i) == 32) {
@@ -26,5 +27,14 @@ public class Solution0058 {
             i--;
         }
         return r - l;
+    }
+
+    public int lengthOfLastWord02(String s) {
+        int end = s.length() - 1;
+        while (end >= 0 && s.charAt(end) == ' ') end--;
+        if (end < 0) return 0;
+        int start = end;
+        while (start >= 0 && s.charAt(start) != ' ') start--;
+        return end - start;
     }
 }
