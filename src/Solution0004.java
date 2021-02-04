485cea08bdc1eb50c1047f92b31cd7c0256d4fc6
shensju <shensju@163.com>
2021-02-04 23:49:58 +0800

    0004 Median of Two Sorted Arrays O(m + n)

---
+++ src/Solution0004.java
@@ -0,0 +1,28 @@
+/**
+ * @Author: shensju
+ * @Date: 2021/2/4 23:06
+ */
+public class Solution0004 {
+
+    /* 0004 Median of Two Sorted Arrays */
+
+    // 时间复杂度：O(m + n)
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int len = m + n;
+        int left = -1, right = -1;
+        int index1 = 0, index2 = 0;
+        for (int i = 0; i <= len / 2; i++) {
+            left = right;
+            if (index1 < m && (index2 >= n || nums1[index1] < nums2[index2])) {
+                right = nums1[index1++];
+            } else {
+                right = nums2[index2++];
+            }
+        }
+        if (len % 2 == 0)
+            return (left + right) / 2.0;
+        return right;
+    }
+}
