8580687c493599017dadf21369afd159b30e204b
shensju <shensju@163.com>
2021-03-19 23:41:50 +0800

    0092 Reverse Linked List II update

--- src/Solution0092.java
+++ src/Solution0092.java
@@ -14,7 +14,7 @@ public class Solution0092 {
     }
 
     /** 0092 Reverse Linked List II **/
-    public ListNode reverseBetween(ListNode head, int left, int right) {
+    public ListNode reverseBetween01(ListNode head, int left, int right) {
         ListNode dummyNode = new ListNode(-1, head);
         ListNode prev = dummyNode;
         for (int i = 0; i < left - 1; i++) {
@@ -53,5 +53,29 @@ public class Solution0092 {
      * 时间复杂度：O(n)  空间复杂度：O(1)
      * Runtime: 0 ms, faster than 100.00% of Java online submissions for Reverse Linked List II.
      * Memory Usage: 36.8 MB, less than 28.17% of Java online submissions for Reverse Linked List II.
+     */
+
+    public ListNode reverseBetween02(ListNode head, int left, int right) {
+        ListNode dummyNode = new ListNode(-1, head);
+        ListNode prev = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            prev = prev.next;
+        }
+        ListNode curr = prev.next;
+        ListNode next;
+        for (int i = 0; i < right - left; i++) {
+            next = curr.next;
+            curr.next = next.next;
+            next.next = prev.next;
+            prev.next = next;
+        }
+        return dummyNode.next;
+    }
+
+    /**
+     * 时间复杂度：O(n)，其中 n 是链表的总结点数，最多只遍历了链表一次，就完成了反转。
+     * 空间复杂度：O(1)，只使用到常数个变量。
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Reverse Linked List II.
+     * Memory Usage: 36.5 MB, less than 66.67% of Java online submissions for Reverse Linked List II.
      */
 }
