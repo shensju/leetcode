5d19712040be59b188802593d41e50e497ed2cbe
shensju <shensju@163.com>
2021-04-20 23:50:47 +0800

    0151 Reverse Words in a String update

--- src/Solution0151.java
+++ src/Solution0151.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @Author: shensju
@@ -20,5 +21,42 @@ public class Solution0151 {
      * 时间复杂度：O(n)  空间复杂度：O(n)
      * Runtime: 9 ms, faster than 26.53% of Java online submissions for Reverse Words in a String.
      * Memory Usage: 38.8 MB, less than 84.67% of Java online submissions for Reverse Words in a String.
+     */
+
+    /**
+     * 用栈实现
+     */
+    public String reverseWords02(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left <= right && s.charAt(left) == ' ')
+            left++;
+        while (left <= right && s.charAt(right) == ' ')
+            right--;
+        Stack<String> stack = new Stack<>();
+        StringBuilder word = new StringBuilder();
+        while (left <= right) {
+            char c = s.charAt(left);
+            if (word.length() != 0 && c == ' ') {
+                stack.push(word.toString());
+                word.setLength(0);
+            } else if (c != ' '){
+                word.append(c);
+            }
+            left++;
+        }
+        stack.push(word.toString());
+        StringBuilder res = new StringBuilder();
+        for (int i = stack.size(); i > 0; i--) {
+            res.append(stack.pop());
+            if (i != 1) res.append(" ");
+        }
+        return res.toString();
+    }
+
+    /**
+     * 时间复杂度：O(n)  空间复杂度：O(n)
+     * Runtime: 4 ms, faster than 83.73% of Java online submissions for Reverse Words in a String.
+     * Memory Usage: 38.4 MB, less than 99.77% of Java online submissions for Reverse Words in a String.
      */
 }
