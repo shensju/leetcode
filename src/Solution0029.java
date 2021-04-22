a719ceec6bf8c9679de7e32a3b516537ed4b1040
shensju <shensju@163.com>
2021-04-22 23:32:19 +0800

    0029 Divide Two Integers update

--- src/Solution0029.java
+++ src/Solution0029.java
@@ -6,31 +6,47 @@ public class Solution0029 {
 
     /** 0029 Divide Two Integers **/
     public int divide(int dividend, int divisor) {
-        if (dividend == 0) return 0;
-        if (divisor == 1) return dividend;
-        if (divisor == -1) return dividend > Integer.MIN_VALUE ? -dividend : Integer.MAX_VALUE;
-        int a = dividend;
-        int b = divisor;
-        int sign = 1;
-        if ((a > 0 && b < 0) || (a < 0 && b > 0)) sign = -1;
-        a = a > 0 ? a : -a;
-        b = b > 0 ? b : -b;
-        int res = div(a, b);
-        if (sign > 0)
-            return res >= Integer.MAX_VALUE ? Integer.MAX_VALUE : res;
-        else
-            return res >= Integer.MAX_VALUE ? Integer.MIN_VALUE : -res;
-    }
-
-    private int div(int a, int b) {
-        if (a < b) return 0;
-        int tb = b;
-        int count = 1;
-        while ((tb + tb) <= a) {
-            count = count + count;
-            tb = tb + tb;
+        // 当除数为1，直接返回被除数
+        if (divisor == 1) {
+            return dividend;
         }
-        return count + divide(a - tb, b);
+        // 当除数为-1且被除数为Integer.MIN_VALUE时，将会溢出，返回Integer.MAX_VALUE
+        if (divisor == -1 && dividend == Integer.MIN_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+
+        // 把被除数与除数调整为正数,为防止被除数Integer.MIN_VALUE转换为正数会溢出，使用long类型保存参数
+        if (dividend < 0 && divisor < 0) {
+            return divide(-(long) dividend, -(long) divisor);
+        } else if (dividend < 0 || divisor < 0) {
+            return -divide(Math.abs((long) dividend), Math.abs((long) divisor));
+        } else {
+            return divide((long) dividend, (long) divisor);
+        }
     }
 
+    public int divide(long dividend, long divisor) {
+        // 如果被除数小于除数，结果明显为0
+        if (dividend < divisor) {
+            return 0;
+        }
+        long sum = divisor; // 记录用了count个divisor的和
+        int count = 1; // 使用了多少个divisor
+        while (dividend >= sum) {
+            // 每次翻倍
+            sum <<= 1;
+            count <<= 1;
+        }
+        // 此时dividend < sum
+        sum >>>= 1;
+        count >>>= 1;
+        // 此时dividend >= sum
+        // 将count个divisor从dividend消耗掉，剩下的还需要多少个divisor交由递归函数处理
+        return count + divide(dividend - sum, divisor);
+    }
+
+    /**
+     * Runtime: 1 ms, faster than 99.97% of Java online submissions for Divide Two Integers.
+     * Memory Usage: 36 MB, less than 72.74% of Java online submissions for Divide Two Integers.
+     */
 }
