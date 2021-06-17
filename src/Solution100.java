94bd2857490873092aa89978a742fda08613c52c
shensju <shensju@163.com>
2021-06-17 23:58:24 +0800

    0100 Same Tree

---
+++ src/Solution100.java
@@ -0,0 +1,108 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * @Author: shensju
+ * @Date: 2021/6/17 23:32
+ * 0100 Same Tree（相同的树）
+ */
+public class Solution100 {
+
+    /** Definition for a binary tree node **/
+    public class TreeNode {
+        private int val;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode() {
+
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * 方法一：广度优先搜索
+     * @param p
+     * @param q
+     * @return
+     */
+    public boolean isSameTree1(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        }
+        Queue<TreeNode> queue1 = new ArrayDeque<>();
+        Queue<TreeNode> queue2 = new ArrayDeque<>();
+        queue1.offer(p);
+        queue2.offer(q);
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            TreeNode node1 = queue1.poll();
+            TreeNode node2 = queue2.poll();
+            if (node1.val != node2.val) {
+                return false;
+            }
+            TreeNode left1 = node1.left;
+            TreeNode right1 = node1.right;
+            TreeNode left2 = node2.left;
+            TreeNode right2 = node2.right;
+            // 使用位异或运算，相同则为false，不相同则为true
+            if (left1 == null ^ left2 == null) {
+                return false;
+            }
+            if (right1 == null ^ right2 == null) {
+                return false;
+            }
+            if (left1 != null) {
+                queue1.offer(left1);
+            }
+            if (right1 != null) {
+                queue1.offer(right1);
+            }
+            if (left2 != null) {
+                queue2.offer(left2);
+            }
+            if (right2 != null) {
+                queue2.offer(right2);
+            }
+        }
+        return queue1.isEmpty() && queue2.isEmpty();
+    }
+    /**
+     * 时间复杂度：O(min(m, n))  空间复杂度：O(min(m, n))
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Same Tree.
+     * Memory Usage: 36.2 MB, less than 69.76% of Java online submissions for Same Tree.
+     */
+
+    /**
+     * 深度优先搜索
+     * @param p
+     * @param q
+     * @return
+     */
+    public boolean isSameTree2(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree2(p.left, q.left) && isSameTree2(p.right, q.right);
+        }
+    }
+    /**
+     * 时间复杂度：O(min(m, n))  空间复杂度：O(min(m, n))
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Same Tree.
+     * Memory Usage: 36.4 MB, less than 56.20% of Java online submissions for Same Tree.
+     */
+}
