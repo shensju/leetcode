e8f31d1a8db987cd6dd96656b52fdb979f4403ae
shensju <shensju@163.com>
2021-10-03 23:53:13 +0800

    queue - 0200_NumIslands

---
+++ src/queue/NumIslands.java
@@ -0,0 +1,47 @@
+package queue;
+
+/**
+ * @Author: shensju
+ * @Date: 2021/10/1 22:56
+ * 0200 岛屿数量
+ */
+public class NumIslands {
+
+    /**
+     * 时间复杂度：O(MN)，其中M和N分别为行数和列数
+     * 空间复杂度：O(MN)，在最坏情况下，整个网格均为陆地，深度优先搜索的深度达到 MN
+     * @param grid
+     * @return
+     */
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+        return num_islands;
+    }
+
+    private void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+}
